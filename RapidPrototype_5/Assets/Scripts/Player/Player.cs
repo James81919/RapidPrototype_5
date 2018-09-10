92fd670744b549cc1cd7dc824704ea30c869f9b7
Enderderder <richardwulansari@gmail.com>
2018-09-10 18:08:23 +1200

    ~ Camera Locking and temp player movement ~ Health bar and player stats

---
+++ RapidPrototype_5/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	[Header("Player Stat")]
+	public float TotalHealth;
+	public float CurrHealth;
+
+
+	private GameObject m_healthBarUI;
+
+	void Awake()
+	{
+		m_healthBarUI = 
+			transform.Find("PlayerUICanvas/PlayerHealthBar").gameObject;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		CurrHealth = TotalHealth;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// Set the health bar to current health by percentage
+		m_healthBarUI.GetComponent<Slider>().value = 
+			CurrHealth / TotalHealth;
+	}
+}
