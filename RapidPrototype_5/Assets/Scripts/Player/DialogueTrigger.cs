f5af8c8ed5e67f0583f0f6946db713d3f664b008
James  Johnstone <jjohnstone2520@gmail.com>
2018-09-10 22:13:35 +1200

    Added textures and added dialogue system [WIP]

---
+++ RapidPrototype_5/Assets/Scripts/Player/DialogueTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public float LetterPauseTime;
+    public float sentencePauseTime;
+    public string popupText = "PRESS E To Interact";
+    public string[] conversationOrder;
+
+    private int currDialogue;
+    private GameObject dialogueBox;
+    private Text conversationText;
+
+    private void Awake()
+    {
+        dialogueBox = GameObject.Find("DialogueBoxPlayer");
+        dialogueBox.SetActive(false);
+        conversationText = dialogueBox.transform.Find("ConversationText").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(PopupDialogue(other.gameObject));
+        }
+    }
+
+    IEnumerator PopupDialogue(GameObject player)
+    {
+        player.GetComponent<PlayerMoveTemp>().enabled = false;
+        player.GetComponent<Rigidbody>().freezeRotation = true;
+
+        dialogueBox.SetActive(true);
+
+        for (int i = 0; i < conversationOrder.Length; i++)
+        {
+            conversationText.text = "";
+            foreach (char item in conversationOrder[i])
+            {
+                yield return new WaitForSeconds(LetterPauseTime);
+                conversationText.text += item;
+            }
+            yield return new WaitForSeconds(sentencePauseTime);
+        }
+
+        dialogueBox.SetActive(false);
+        player.GetComponent<Rigidbody>().freezeRotation = false;
+        player.GetComponent<PlayerMoveTemp>().enabled = true;
+    }
+}
