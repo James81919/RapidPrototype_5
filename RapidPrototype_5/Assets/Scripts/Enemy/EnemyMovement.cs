346525ac6fb4450c9367170ac5f6b55ea897dba1
James  Johnstone <jjohnstone2520@gmail.com>
2018-09-11 11:41:49 +1200

    Added enemy which wanders around

---
+++ RapidPrototype_5/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float minXWanderDistance;
+    public float maxXWanderDistance;
+    public float minZWanderDistance;
+    public float maxZWanderDistance;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(MoveToPos());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator MoveToPos()
+    {
+        Vector3 newPos = new Vector3(Random.Range(minXWanderDistance, maxXWanderDistance),
+            transform.position.y, Random.Range(minZWanderDistance,
+            maxZWanderDistance));
+
+        agent.SetDestination(newPos);
+        yield return new WaitForSeconds(3);
+        StartCoroutine(MoveToPos());
+    }
+}
