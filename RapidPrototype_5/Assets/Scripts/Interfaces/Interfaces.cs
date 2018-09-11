b4a65f81e6ec3ef06ac0ef6b9f3179bf1ec17d50
Enderderder <richardwulansari@gmail.com>
2018-09-12 02:45:43 +1200

    ~ Added rock gameobject and its behaviours

--- RapidPrototype_5/Assets/Scripts/Interfaces/Interfaces.cs
+++ RapidPrototype_5/Assets/Scripts/Interfaces/Interfaces.cs
@@ -2,16 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public interface IKillable
 {
     void TakeDamage(float _value);
     void CheckDeath();
     void KillEntity();
     bool IsAlive();
-}
-
-public interface ICombatEntity
-{
-
 }
---
+++ RapidPrototype_5/Assets/Scripts/Rock/Rock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour, IKillable
+{
+    [Header("Stats")]
+    public float TotalHealth = 1f;
+    public float CurrHealth;
+
+	void Start ()
+    {
+        // Set to full health at the beginning
+        CurrHealth = TotalHealth;
+    }
+
+    /* Interface Implementation =================================*/
+    
+    // IKillable
+    public void TakeDamage(float _value)
+    {
+        CurrHealth -= _value;
+        CheckDeath();
+    }
+    public void CheckDeath()
+    {
+        if (IsAlive() == false)
+        {
+            KillEntity();
+        }
+    }
+    public void KillEntity()
+    {
+        Debug.Log("Rock Dead");
+
+        /// Play Rock death animation
+
+        /// Spawn the blue soul crystal
+        
+        /// De-spawn the object
+    }
+    public bool IsAlive()
+    {
+        if (CurrHealth <= 0f)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+    // ============================================================
+}
