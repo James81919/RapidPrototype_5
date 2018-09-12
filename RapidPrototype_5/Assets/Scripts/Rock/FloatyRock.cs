133e611e2db8f36525dc88a30465032874b5342e
Enderderder <richardwulansari@gmail.com>
2018-09-13 02:30:50 +1200

    ~ Fixed a bug in pause menu ~ Create Floating Rocks and the combination StrangeBigRock ~ Create the final build scene "Main Menu" (Done)

---
+++ RapidPrototype_5/Assets/Scripts/Rock/FloatyRock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatyRock : MonoBehaviour
+{
+    [Header("Config")]
+    public float FloatingRange = 0.5f;
+    public float FloatingSpeed = 2f;
+    public float StartingOffset;
+
+    private Vector3 m_beginningPos;
+
+	void Start ()
+    {
+        // Set the beginning position
+        m_beginningPos = this.transform.position;
+    }
+	
+	void Update ()
+    {
+        // Sine Wave
+        float y = FloatingRange * Mathf.Sin( (Time.time * FloatingSpeed) + StartingOffset );
+
+        Vector3 resultVec = m_beginningPos;
+        resultVec.y += y;
+        this.transform.position = resultVec;
+
+    }
+}
